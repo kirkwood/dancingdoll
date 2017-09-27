005a0743b1d0b8759210c7f089bb96d66542c17e
C.R. Kirkwood-Watts <kirkwood@totzeit.net>
2017-09-27 08:54:22 -0700

    Some simplification.

--- commtest/src/main/scala/coop/rchain/kademlia/kademlia.scala
+++ commtest/src/main/scala/coop/rchain/kademlia/kademlia.scala
@@ -6,11 +6,11 @@ trait Keyed {
   def key: Array[Byte]
 }
 
-trait Remote[A] {
+trait Remote {
   def ping: Unit
 }
 
-trait Peer[A] extends Remote[A] with Keyed
+trait Peer extends Remote with Keyed
 
 trait Latent {
   def latency: Double
@@ -28,21 +28,15 @@ object ReputationOrder extends Ordering[Reputable] {
   def compare(a: Reputable, b: Reputable) = a.reputation compare b.reputation
 }
 
-class PeerNode(val pKey: Array[Byte]) extends Keyed {
-  val key = pKey
-  lazy private val sKey = key map { "%02x" format _ } mkString
-
-  override def toString = s"#{PeerNode ${sKey}}"
-}
-
-
 class PeerTableEntry[A <: Keyed](val entry: A) extends Keyed {
   override def key = entry.key
+
+  override def toString = s"#{PeerTableEntry $entry}"
 }
 
 object NodeTable {
-  // Number of bits considered in the distance function.
-  val width = 256
+  // Number of bits considered in the distance function. Taken from the passed-in "home" value to the table.
+  // val width = 256
 
   // Maximum length of each row of the routing table.
   val redundancy = 20
@@ -58,22 +52,36 @@ object NodeTable {
   // val bucketWidth = 1
 }
 
-class PeerTable[A <: Keyed](self: A) {
-  val table = new Array[MutableList[PeerTableEntry[A]]](NodeTable.width)
+class PeerTable[A <: Keyed](home: A, val k: Int = NodeTable.redundancy, val alpha: Int = NodeTable.alpha) {
+  val width = 8*home.key.size
+  val table = new Array[MutableList[PeerTableEntry[A]]](width)
   // val byLatency = PriorityQueue.empty(LatencyOrder.reverse)
   // val byReputation = PriorityQueue.empty(ReputationOrder)
   val pending = new HashMap[Array[Byte], (A, A)]
 
   private def ping(older: A, newer: A): Unit = {
-    println(s"Pinging $older, might replace with $newer")
-    pending(older.key) = (older, newer)
+    // TODO constrain to alpha in flight
+    println(s"STUB: Pinging $older, might replace with $newer.")
+    pending synchronized {
+      pending.get(older.key) match {
+        case Some(_) => {
+          // Protocol error -- need throttling
+          println(s"Multiple pings in flight for $older.")
+        }
+        case None => pending(older.key) = (older, newer)
+      }
+    }
   }
 
   // Kademlia XOR distance function.
   def distance(a: A, b: A): Int = {
     // TODO: ensure keys same length
+    if (a == b || a.key == b.key) {
+      return width
+    }
+
     var dist = 0
-    for (i <- 0 to a.key.size - 1) {
+    for (i <- 0 to width - 1) {
       if (a.key(i) == b.key(i)) {
         dist += 8
       } else {
@@ -89,22 +97,24 @@ class PeerTable[A <: Keyed](self: A) {
   }
 
   def add(a: A) {
-    val ind = distance(self, a)
+    val ind = distance(home, a)
     // TODO: Bounds check 0 <= ind < table.size
-    table(ind) match {
-      case null => {
-        table(ind) = MutableList[PeerTableEntry[A]](new PeerTableEntry[A](a))
-        // byLatency += a
-        // byReputation += a
-      }
-      case l => {
-        if (l.size < NodeTable.redundancy) {
-          l += new PeerTableEntry[A](a)
-        } else {
-          // ping first (oldest) element; if it responds, move it to back
-          // (newest); if it doesn't respond, remove it and place a in
-          // back
-          ping(l(0).entry, a)
+    table synchronized {
+      table(ind) match {
+        case null => {
+          table(ind) = MutableList[PeerTableEntry[A]](new PeerTableEntry[A](a))
+          // byLatency += a
+          // byReputation += a
+        }
+        case l => {
+          if (l.size < k) {
+            l += new PeerTableEntry[A](a)
+          } else {
+            // ping first (oldest) element; if it responds, move it to back
+            // (newest); if it doesn't respond, remove it and place a in
+            // back
+            ping(l(0).entry, a)
+          }
         }
       }
     }
